16223626068db2d7d2450fe4cbb78aa81d21e709
Vlad Victor Ungureanu <ungureanuvladvictor@gmail.com>
2013-06-13 01:31:17 +0300

    IPv4 added

---
+++ ipv4.h
@@ -0,0 +1,14 @@
+#include <linux/ip.h>
+#include <syslog.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+      
+#include "udp.h"
+#include "bootp.h"
+
+void make_ipv4(struct iphdr *ip, char *src_addr, char *dst_addr, uint8_t proto);
+uint16_t ip_checksum(const void *buf, size_t hdr_len);;
+void debug_ipv4(struct iphdr *ip);
+
