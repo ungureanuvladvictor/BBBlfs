1cf1f72ee26a0701886a388f0fe6d16e7298db7c
Vlad Victor Ungureanu <ungureanuvladvictor@gmail.com>
2013-06-13 15:02:23 +0300

    Cosmetic fix

--- udp.c
+++ udp.c
@@ -20,8 +20,8 @@ void make_udp(udp_t *udp_packet, uint16_t udp_len, uint16_t srcPort, uint16_t ds
 
 void debug_udp(udp_t *udp_packet, uint16_t udp_len) {
 	syslog(LOG_DEBUG, "Starting UDP Log");
-	syslog(LOG_DEBUG, "Src Port: %d",udp_packet->udpSrc);
-	syslog(LOG_DEBUG, "Dst Port: %d",udp_packet->udpDst);
+	syslog(LOG_DEBUG, "Src Port: %d", udp_packet->udpSrc);
+	syslog(LOG_DEBUG, "Dst Port: %d", udp_packet->udpDst);
 	syslog(LOG_DEBUG, "Length: %d", udp_packet->udpLen);
 	syslog(LOG_DEBUG, "CheckSum: %d", udp_packet->chkSum);
 	syslog(LOG_DEBUG, "End of UDP Log");
