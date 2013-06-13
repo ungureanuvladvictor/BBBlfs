a43ea1e013db141c0fc8626b7e7908fe02521337
Vlad Victor Ungureanu <ungureanuvladvictor@gmail.com>
2013-06-13 15:38:16 +0300

    Fixed endian problem

--- udp.c
+++ udp.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <syslog.h>
+#include <inttypes.h>
 
 #include "udp.h"
 
@@ -20,8 +21,8 @@ void make_udp(udp_t *udp_packet, uint16_t udp_len, uint16_t srcPort, uint16_t ds
 
 void debug_udp(udp_t *udp_packet, uint16_t udp_len) {
 	syslog(LOG_DEBUG, "Starting UDP Log");
-	syslog(LOG_DEBUG, "Src Port: %d", udp_packet->udpSrc);
-	syslog(LOG_DEBUG, "Dst Port: %d", udp_packet->udpDst);
+	syslog(LOG_DEBUG, "Src Port: %d", ntohs(udp_packet->udpSrc));
+	syslog(LOG_DEBUG, "Dst Port: %d", ntohs(udp_packet->udpDst));
 	syslog(LOG_DEBUG, "Length: %d", udp_packet->udpLen);
 	syslog(LOG_DEBUG, "CheckSum: %d", udp_packet->chkSum);
 	syslog(LOG_DEBUG, "End of UDP Log");
