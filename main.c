fee1abeddb6eeb5f08fdfa9c258b75a9c6e7d4c6
Vlad Victor Ungureanu <ungureanuvladvictor@gmail.com>
2013-06-14 04:34:39 +0300

    needs testing

--- main.c
+++ main.c
@@ -9,13 +9,23 @@
 #include "ipv4.h"
 #include "ether2.h"
 #include "rndis.h"
+#include "utils.h"
 
 void hexDump (char *desc, void *addr, int len);
 
 int main(int argc, const char * argv[]) {
-    unsigned char *data = (unsigned char*)malloc(800*sizeof(unsigned char));
     int actual;
     
+    ssize_t fullSize = sizeof(bootp_packet) + sizeof(udp_t) + sizeof(struct iphdr) + sizeof(struct ethhdr) + sizeof(rndis_hdr);
+    ssize_t rndisSize = sizeof(rndis_hdr);
+    ssize_t etherSize = sizeof(struct ethhdr);
+    ssize_t ipSize = sizeof(struct iphdr);
+    ssize_t udpSize = sizeof(udp_t);
+    ssize_t bootpSize = sizeof(bootp_packet);
+
+    unsigned char *data = (unsigned char*)calloc(1, fullSize);
+    unsigned char *buffer = (unsigned char*)malloc(450 * sizeof(unsigned char));
+
     libusb_device **devs; 
     libusb_device_handle *dev_handle;
     libusb_context *ctx = NULL;
@@ -53,110 +63,111 @@ int main(int argc, const char * argv[]) {
         return 1;
     }
     
-    while(libusb_bulk_transfer(dev_handle, (129 | LIBUSB_ENDPOINT_IN), data, 800, &actual, 0) ==0) {
-        printf("Got %d\n",actual);
-        rndis_hdr *rndis = calloc(1,sizeof(rndis_hdr));
-        rndis = (rndis_hdr*)data;
-        debug_rndis(rndis);
-        
-        struct ethhdr *eth2 = calloc(1,sizeof(struct ethhdr));
-        eth2 = (struct ethhdr*)(data+sizeof(rndis_hdr));
-        debug_ether2(eth2);
+    r = libusb_bulk_transfer(dev_handle, (129 | LIBUSB_ENDPOINT_IN), 
+    							buffer, 450, &actual, 0);
 
-        struct iphdr *ip = calloc(1, sizeof(struct iphdr));
-        ip = (struct iphdr*)(data + sizeof(rndis_hdr) + sizeof(struct ethhdr));
-        debug_ipv4(ip);
-
-        udp_t *udp = calloc(1, sizeof(udp_t));
-        udp = (udp_t*)(data + sizeof(rndis_hdr) + sizeof(struct ethhdr) + sizeof(struct iphdr));
-        debug_udp(udp, sizeof(udp_t));
-        
-        bootp_packet *bootp = calloc(1, sizeof(bootp_packet));
-        bootp = (bootp_packet*)(data + sizeof(rndis_hdr) + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(udp_t));
-        debug_bootp(bootp, sizeof(bootp_packet));
-
-        free(data);
-        data = (unsigned char*)malloc(800*sizeof(unsigned char));
-    }
+    hexDump("Received", buffer, 450);
+    bootp_packet *breq = (bootp_packet*)calloc(1, sizeof(bootp_packet));
+    make_bootp(servername, filename, breq);
     
+    udp_t *udp = (udp_t*)calloc(1, sizeof(udp_t));
+    make_udp(udp, sizeof(bootp_packet), 67, 68);
+    
+    struct iphdr *ip = (struct iphdr*)calloc(1, sizeof(struct iphdr));
+    make_ipv4(ip, server_ip, BBB_ip, 17, 0);
+    
+    struct ethhdr *ether = (struct ethhdr*)calloc(1, sizeof(struct ethhdr));
+    memcpy(ether->h_dest, BBB_hwaddr, 6);
+    memcpy(ether->h_source, my_hwaddr, 6);
+    ether->h_proto = ETH_P_IP;
+
+    rndis_hdr *receivedRndis = (rndis_hdr*)buffer;
+
+    hexDump("RNDIS Received", receivedRndis, rndisSize);
+    debug_rndis(receivedRndis);
+    rndis_hdr *rndis = (rndis_hdr*)calloc(1, sizeof(rndis_hdr));
+    make_rndis(rndis, fullSize - sizeof(rndis_hdr));
+    debug_rndis(rndis);
+    hexDump("RNDIS", rndis, rndisSize);
+    
+    memcpy(data, rndis, rndisSize);
+    memcpy(data + rndisSize, ether, etherSize);
+    memcpy(data + rndisSize + etherSize, ip, ipSize);
+    memcpy(data + rndisSize + etherSize + ipSize, udp, udpSize);
+    memcpy(data + rndisSize + etherSize + ipSize + udpSize, breq, bootpSize);
+    
+    /*r = libusb_bulk_transfer(dev_handle, (2 | LIBUSB_ENDPOINT_OUT), 
+                                data, fullSize, &actual, 0);
+
+    r = libusb_bulk_transfer(dev_handle, (129 | LIBUSB_ENDPOINT_IN),
+                                buffer, 450, &actual, 0);
+    printf("%d\n", actual);
+    hexDump("After sending", buffer, actual);*/
+
     r = libusb_release_interface(dev_handle, 1); 
     if(r!=0) {
         printf("Cannot release interface!\n");
         return 1;
     }
+
     libusb_close(dev_handle); 
     libusb_exit(ctx);
-    free(data);
-
-    /*
-    bootp_packet *breq = (bootp_packet *)malloc(sizeof(bootp_packet));
-    make_bootp(servername, filename, breq);
-    debug_bootp(breq, sizeof(*breq));
-    
-    udp_t *udp = (udp_t *)malloc(sizeof(udp_t));
-    make_udp(udp, 30, 67, 68);
-    debug_udp(udp, udp->udpLen);
-    free(udp);
-
-    struct iphdr *ip = (struct iphdr*)malloc(sizeof(struct iphdr)+sizeof(bootp_packet)+sizeof(udp_t));
-    make_ipv4(ip,"192.168.0.1","192.168.0.2",17);
-    debug_ipv4(ip);
+    free(ether);
+    free(breq);
     free(ip);
+    free(udp);
+    free(data);
+    free(buffer);
 
-    u_int8_t dst[6] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
-    u_int8_t src[6] = {0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c};
-    struct ethhdr *eth2 = (struct ethhdr*)malloc(sizeof(struct ethhdr));
-    make_ether2(eth2, dst, src);
-    debug_ether2(eth2);
-    free(eth2);
-
-    rndis_hdr *rndis = (rndis_hdr*)calloc(1,sizeof(rndis_hdr));
-    make_rndis(rndis, 10);
-    debug_rndis(rndis);
-    free(rndis);
-    */
     return 0;
 }
+/*
+struct ethhdr *eth2 = calloc(1, sizeof(struct ethhdr));
+    eth2 = (struct ethhdr*)(data + sizeof(rndis_hdr));
+       
+    struct iphdr *ip = calloc(1, sizeof(struct iphdr));
+    ip = (struct iphdr*)(data + sizeof(rndis_hdr) + sizeof(struct ethhdr));
+       
+    udp_t *udp = calloc(1, sizeof(udp_t));
+    udp = (udp_t*)(data + sizeof(rndis_hdr) + sizeof(struct ethhdr) 
+                        + sizeof(struct iphdr));
+        
+    bootp_packet *bootp = calloc(1, sizeof(bootp_packet));
+    bootp = (bootp_packet*)(data + sizeof(rndis_hdr) + sizeof(struct ethhdr)
+                                + sizeof(struct iphdr) + sizeof(udp_t));
+        
+    bootp_packet *response = (bootp_packet*)malloc(sizeof(bootp_packet));
+    make_bootp(servername, filename, response);
+    memcpy(&response->hwaddr, &bootp->hwaddr, 6);
+       
+    udp_t *udp_answer = (udp_t*)malloc(sizeof(udp_t));
+    make_udp(udp_answer, sizeof(bootp_packet), udp->udpDst, udp->udpSrc);
+       
+    struct iphdr *ip_answer = (struct iphdr*)calloc(1, sizeof(struct iphdr) 
+                                + sizeof(udp_t) + sizeof(bootp_packet));
+    make_ipv4(ip_answer, "255.255.255.0", "0.0.0.0", 17, ip->id);
+        
+    unsigned char srcHwAddr[] = {0xc8, 0xbc, 0xc8, 0xe3, 0x5b, 0xac};
+    struct ethhdr *eth_answer = (struct ethhdr*)calloc(1, sizeof(struct ethhdr));
+    memcpy(&eth_answer->h_source, &srcHwAddr, 6);
+    memcpy(&eth_answer->h_dest, &eth2->h_source, 6);
+    eth_answer->h_proto = ETH_P_IP;
+        
+    rndis_hdr *rndis_answer = (rndis_hdr*)calloc(1, sizeof(rndis_hdr) 
+        + sizeof(struct ethhdr) + sizeof(struct iphdr) 
+        + sizeof(udp_t) + sizeof(bootp_packet));
+    make_rndis(rndis_answer, sizeof(struct ethhdr) 
+        + sizeof(struct iphdr) + sizeof(udp_t) + sizeof(bootp_packet));
+    
+    unsigned char* dump = (unsigned char*)malloc(800*sizeof(unsigned char));
+    memcpy(dump, rndis_answer, sizeof(rndis_hdr));
+    memcpy((dump+sizeof(rndis_hdr)), eth_answer, sizeof(struct ethhdr));
+    memcpy(dump + sizeof(rndis_hdr) + sizeof(struct ethhdr), ip_answer, 
+            sizeof(struct iphdr));
+    memcpy(dump + sizeof(rndis_hdr) + sizeof(struct ethhdr) 
+            + sizeof(struct iphdr), udp_answer, sizeof(udp_t));
+    memcpy(dump + sizeof(rndis_hdr) + sizeof(struct ethhdr) 
+        + sizeof(struct iphdr) + sizeof(udp_t), response, sizeof(bootp_packet));
+    hexDump("Packet", dump, 384);
 
-void hexDump (char *desc, void *addr, int len) {
-    int i;
-    unsigned char buff[17];
-    unsigned char *pc = addr;
-
-    // Output description if given.
-    if (desc != NULL)
-        printf ("%s:\n", desc);
-
-    // Process every byte in the data.
-    for (i = 0; i < len; i++) {
-        // Multiple of 16 means new line (with line offset).
-
-        if ((i % 16) == 0) {
-            // Just don't print ASCII for the zeroth line.
-            if (i != 0)
-                printf ("  %s\n", buff);
-
-            // Output the offset.
-            printf ("  %04x ", i);
-        }
-
-        // Now the hex code for the specific character.
-        printf (" %02x", pc[i]);
-
-        // And store a printable ASCII character for later.
-        if ((pc[i] < 0x20) || (pc[i] > 0x7e))
-            buff[i % 16] = '.';
-        else
-            buff[i % 16] = pc[i];
-        buff[(i % 16) + 1] = '\0';
-    }
-
-    // Pad out last line if not exactly 16 characters.
-    while ((i % 16) != 0) {
-        printf ("   ");
-        i++;
-    }
-
-    // And print the final ASCII bit.
-    printf ("  %s\n", buff);
-}
+*/
