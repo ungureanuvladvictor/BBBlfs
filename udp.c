9368c044c0d4a4148d37ae0bb2fc76ecba937867
Vlad Victor Ungureanu <ungureanuvladvictor@gmail.com>
2013-06-14 04:33:49 +0300

    minor fix

--- udp.c
+++ udp.c
@@ -8,18 +8,19 @@
 
 #include "udp.h"
 
-void make_udp(udp_t *udp_packet, uint16_t udp_len, uint16_t srcPort, uint16_t dstPort) {
+void make_udp(udp_t *udp_packet, uint16_t udp_len, 
+			uint16_t srcPort, uint16_t dstPort) {
 	memset(&udp_packet->chkSum, 0, sizeof(udp_packet->chkSum));
 	memset(&udp_packet->udpLen, 0, sizeof(udp_packet->udpLen));
 
-	udp_packet->udpSrc = htons(srcPort);
-	udp_packet->udpDst = htons(dstPort); 
+	udp_packet->udpSrc = srcPort;
+	udp_packet->udpDst = dstPort; 
 
 	udp_packet->udpLen += 8;
 	udp_packet->udpLen += udp_len;
 }
 
-void debug_udp(udp_t *udp_packet, uint16_t udp_len) {
+void debug_udp(udp_t *udp_packet) {
 	syslog(LOG_DEBUG, "Starting UDP Log");
 	syslog(LOG_DEBUG, "Src Port: %d", ntohs(udp_packet->udpSrc));
 	syslog(LOG_DEBUG, "Dst Port: %d", ntohs(udp_packet->udpDst));
