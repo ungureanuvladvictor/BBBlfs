2bd8c47dd30cc43f35269ef5ad6d57a7b7dd6f38
Vlad Victor Ungureanu <ungureanuvladvictor@gmail.com>
2013-06-15 01:29:38 +0300

    modified length parameter, use inet_aton

--- ipv4.c
+++ ipv4.c
@@ -10,17 +10,17 @@
 
 uint16_t ip_checksum(const void *buf, size_t hdr_len);
 
-void make_ipv4(struct iphdr *ip, char *src_addr, char *dst_addr, uint8_t proto, uint16_t id) {
+void make_ipv4(struct iphdr *ip, char *src_addr, char *dst_addr, uint8_t proto, uint16_t id, ssize_t total_len) {
 	ip->version = 4;
 	ip->ihl = 5;
 	ip->ttl = 64;
-	ip->id = id;
-	ip->tot_len = htons((sizeof(struct iphdr) + sizeof(udp_t) + sizeof(bootp_packet)));
+	ip->id = htons(id);
+	ip->tot_len = htons(total_len);
 	ip->protocol = proto;
-	if(inet_aton("192.168.1.9", &ip->saddr) == NULL) {
+	if(inet_aton("192.168.1.9", &ip->saddr) == 0) {
 		printf("Cannot add IPv4 src address!\n");
 	}
-	if(inet_aton("192.168.1.3", &ip->daddr) == NULL) {
+	if(inet_aton("192.168.1.3", &ip->daddr) == 0) {
 		printf("Cannot add IPv4 dst address!\n");
 	}
 	ip->check = ip_checksum(ip, sizeof(struct iphdr)); 
