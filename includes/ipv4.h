e546d5e3feea23a838ba56de44cbe60f7e1cec35
Jason Kridner <jdk@ti.com>
2014-08-12 13:16:21 -0400

    Ported to Apple Mac OS X
    * Using MacPorts libusb:
      http://www.macports.org/ports.php?by=library&substr=libusb
      version: 1.0.18
    * Simply adjusted headers
    * Not yet tested

--- includes/ipv4.h
+++ includes/ipv4.h
@@ -14,7 +14,13 @@
  *  limitations under the License.
  */
 
+#ifdef __APPLE__
+#include <netinet/ip.h>
+typedef struct ip iphdr_t;
+#else
 #include <linux/ip.h>
+typedef struct iphdr iphdr_t;
+#endif
 #include <syslog.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,7 +30,7 @@
 #include "udp.h"
 #include "bootp.h"
 
-void make_ipv4(struct iphdr *ip, const char *src_addr, const char *dst_addr,
+void make_ipv4(iphdr_t *ip, const char *src_addr, const char *dst_addr,
                 uint8_t proto, uint16_t id, ssize_t total_len);
 uint16_t ip_checksum(const void *buf, size_t hdr_len);;
-void debug_ipv4(struct iphdr *ip);
+void debug_ipv4(iphdr_t *ip);
