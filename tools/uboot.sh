363cb6f45aabccbef0088385738a3abc235d5970
Fernando Ortiz <fortiz2k@gmail.com>
2015-02-20 21:41:24 -0600

    the patch applies cleanly on this tag

--- tools/uboot.sh
+++ tools/uboot.sh
@@ -12,6 +12,7 @@ echo Building custom U-Boot
 git clone git://git.denx.de/u-boot.git /tmp/u-boot
 pushd /tmp/u-boot
   git reset --hard
+  git checkout v2014.07 -b tmp
   wget -c https://raw.githubusercontent.com/ungureanuvladvictor/BBBlfs/master/tools/USB_FLash.patch
   patch -p1 < USB_FLash.patch
   make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE am335x_evm_usbspl_defconfig
