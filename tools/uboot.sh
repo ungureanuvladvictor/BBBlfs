6177d960b2f8ee166db10865364b366a5f3d2463
Vlad Victor Ungureanu <vvu@vdev.ro>
2015-02-22 04:45:12 +0100

    Using linaro CC locally.
    Signed-off-by: Vlad Victor Ungureanu <vvu@vdev.ro>

--- tools/uboot.sh
+++ tools/uboot.sh
@@ -15,7 +15,8 @@ pushd /tmp/u-boot
   git checkout 524123a70761110c5cf3ccc5f52f6d4da071b959
   wget -c https://raw.githubusercontent.com/ungureanuvladvictor/BBBlfs/master/tools/USB_FLash.patch
   patch -p1 < USB_FLash.patch
-  make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE am335x_evm_usbspl_defconfig
+  make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE distclean
+  make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE am335x_evm_usbspl_config
   make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE
   cp -p u-boot.img $current/uboot
   cp -p spl/u-boot-spl.bin $current/spl
