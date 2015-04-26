050ac6ec519e2716c60e789d96b5730a2a7847cd
Vlad Ungureanu <vvu@vdev.ro>
2015-04-26 06:49:48 +0200

    Needs to match definition in .h file.
    Signed-off-by: Vlad Ungureanu <vvu@vdev.ro>

--- src/ipv4.c
+++ src/ipv4.c
@@ -26,7 +26,7 @@
 
 uint16_t ip_checksum(const void *buf, size_t hdr_len);
 
-void make_ipv4(struct iphdr *ip, char *src_addr, char *dst_addr,
+void make_ipv4(struct iphdr *ip, const char *src_addr, const char *dst_addr,
                 uint8_t proto, uint16_t id, ssize_t total_len) {
 	ip->version = 4;
 	ip->ihl = 5;
