0db89e7839647d22698bcae4a1e5519ef28dd0ea
Vlad Ungureanu <vvu@vdev.ro>
2015-05-03 22:30:55 +0200

    RNDIS headers and functions added.
    Added defines, structs and functions for RNDIS init and set filter.
    
    Signed-off-by: Vlad Ungureanu <vvu@vdev.ro>

--- include/rndis.h
+++ include/rndis.h
@@ -18,6 +18,61 @@
 #define RNDIS_H
 
 #include <stdint.h>
+#include <libusb.h>
+
+#if defined(__BIG_ENDIAN__) || defined(_BIG_ENDIAN)
+
+#define le32_to_cpu(x) (\
+	(((x)>>24)&0xff)\
+	|\
+	(((x)>>8)&0xff00)\
+	|\
+	(((x)<<8)&0xff0000)\
+	|\
+	(((x)<<24)&0xff000000)\
+)
+
+#define le16_to_cpu(x) ( (((x)>>8)&0xff) | (((x)<<8)&0xff00) )
+
+#else
+
+#define le32_to_cpu(x) (x)
+#define le16_to_cpu(x) (x)
+
+#endif
+
+#define cpu_to_le32(x) le32_to_cpu(x)
+#define cpu_to_le16(x) le16_to_cpu(x)
+
+#define RNDIS_MSG_INIT						0x00000002
+#define USB_TYPE_CLASS                     	(0x01 << 5)
+#define USB_CDC_SEND_ENCAPSULATED_COMMAND  	0x00
+#define USB_RECIP_INTERFACE                	0x01
+#define USB_DIR_IN							0x80
+#define USB_CDC_GET_ENCAPSULATED_RESPONSE  	0x01
+#define USB_RECIP_INTERFACE                	0x01
+
+#define	CONTROL_BUFFER_SIZE					1025
+#define RNDIS_MSG_SET						0x00000005
+#define	RNDIS_CONTROL_TIMEOUT_MS			(1000)
+#define RNDIS_OID_GEN_CURRENT_PACKET_FILTER	0x0001010E
+#define RNDIS_PACKET_TYPE_DIRECTED			0x00000001
+#define RNDIS_PACKET_TYPE_MULTICAST			0x00000002
+#define RNDIS_PACKET_TYPE_ALL_MULTICAST		0x00000004
+#define RNDIS_PACKET_TYPE_BROADCAST			0x00000008
+#define RNDIS_PACKET_TYPE_SOURCE_ROUTING	0x00000010
+#define RNDIS_PACKET_TYPE_PROMISCUOUS		0x00000020
+#define RNDIS_PACKET_TYPE_SMT				0x00000040
+#define RNDIS_PACKET_TYPE_ALL_LOCAL			0x00000080
+#define RNDIS_PACKET_TYPE_GROUP				0x00001000
+#define RNDIS_PACKET_TYPE_ALL_FUNCTIONAL	0x00002000
+#define RNDIS_PACKET_TYPE_FUNCTIONAL		0x00004000
+#define RNDIS_PACKET_TYPE_MAC_FRAME			0x00008000
+#define RNDIS_DEFAULT_FILTER ( \
+	RNDIS_PACKET_TYPE_DIRECTED | \
+	RNDIS_PACKET_TYPE_BROADCAST | \
+	RNDIS_PACKET_TYPE_ALL_MULTICAST | \
+	RNDIS_PACKET_TYPE_PROMISCUOUS)
 
 typedef struct {
 	uint32_t msg_type; /*!< Always 1*/
@@ -31,9 +86,34 @@ typedef struct {
 	uint32_t packet_info_len; /*!< Not used in this project*/
 	uint32_t reserved_first; /*!< Not used in this project*/
 	uint32_t reserved_second; /*!< Not used in this project*/
-} rndis_hdr;
+} rndis_data_hdr;
 
-void make_rndis(rndis_hdr *rndishdr, uint32_t data_length);
-void debug_rndis(rndis_hdr *rndis);
+typedef struct {
+	uint32_t msg_type;
+	uint32_t msg_len;
+	uint32_t request_id;
+	uint32_t major_version;
+	uint32_t minor_version;
+	uint32_t max_transfer_size;
+} rndis_init_hdr;
+
+typedef struct {
+	uint32_t msg_type;
+	uint32_t msg_len;
+	uint32_t request_id;
+	uint32_t oid;
+	uint32_t len;
+	uint32_t offset;
+	uint32_t reserved;
+} rndis_set_hdr;
+
+void make_rndis_data_hdr(rndis_data_hdr *rndis_data, uint32_t data_length);
+void make_rndis_init_hdr(rndis_init_hdr *rndis_init);
+void make_rndis_set_hdr(rndis_set_hdr *rndis_set);
+int rndis_send_init(libusb_device_handle *dev_handle, unsigned char *buf,
+					int buflen);
+int rndis_send_filter(libusb_device_handle *dev_handle, unsigned char *buf,
+					int buflen);
+void debug_rndis(rndis_data_hdr *rndis);
 
 #endif
--- src/rndis.c
+++ src/rndis.c
@@ -19,15 +19,81 @@
 #include <syslog.h>
 
 #include "rndis.h"
+#include "utils.h"
 
-void make_rndis(rndis_hdr *rndishdr, uint32_t data_length) {
-	rndishdr->data_len = data_length;
-	rndishdr->data_offset = 0x24;
-	rndishdr->msg_type = 0x00000001;
-	rndishdr->msg_len = data_length + 44;
+int rndis_send_init(libusb_device_handle *dev_handle, unsigned char *buf,
+					int buflen) {
+	int r = 0;
+	r = libusb_control_transfer(dev_handle,
+								USB_TYPE_CLASS | USB_RECIP_INTERFACE,
+								USB_CDC_SEND_ENCAPSULATED_COMMAND, 0, 0, buf,
+								buflen, RNDIS_CONTROL_TIMEOUT_MS);
+	if (r < 0) {
+		return r;
+	}
+
+	r = libusb_control_transfer(dev_handle,
+								USB_DIR_IN | USB_TYPE_CLASS |
+								USB_RECIP_INTERFACE,
+								USB_CDC_GET_ENCAPSULATED_RESPONSE, 0, 0, buf,
+								CONTROL_BUFFER_SIZE, RNDIS_CONTROL_TIMEOUT_MS);
+	if (r < 0) {
+		return r;
+	}
+
+	return r;
+}
+int rndis_send_filter(libusb_device_handle *dev_handle, unsigned char *buf,
+						int buflen) {
+	int r = 0;
+	r = libusb_control_transfer(dev_handle, USB_TYPE_CLASS |
+								USB_RECIP_INTERFACE,
+								USB_CDC_SEND_ENCAPSULATED_COMMAND, 0, 0, buf,
+								buflen, RNDIS_CONTROL_TIMEOUT_MS);
+	if (r < 0) {
+		return r;
+	}
+
+	r = libusb_control_transfer(dev_handle, USB_DIR_IN | USB_TYPE_CLASS |
+								USB_RECIP_INTERFACE,
+								USB_CDC_GET_ENCAPSULATED_RESPONSE, 0, 0, buf,
+								CONTROL_BUFFER_SIZE, RNDIS_CONTROL_TIMEOUT_MS);
+	if (r < 0) {
+		return r;
+	}
+
+	return r;
 }
 
-void debug_rndis(rndis_hdr *rndis) {
+void make_rndis_data_hdr(rndis_data_hdr *rndis_data, uint32_t data_length) {
+	rndis_data->data_len = data_length;
+	rndis_data->data_offset = 0x24;
+	rndis_data->msg_type = 0x00000001;
+	rndis_data->msg_len = data_length + 44;
+}
+
+void make_rndis_init_hdr(rndis_init_hdr *rndis_init) {
+	rndis_init->msg_type = cpu_to_le32(RNDIS_MSG_INIT);
+	rndis_init->msg_len = cpu_to_le32(0x00000018);
+	rndis_init->major_version = cpu_to_le32(1);
+	rndis_init->request_id = cpu_to_le32(1);
+	rndis_init->minor_version = cpu_to_le32(0);
+	rndis_init->max_transfer_size = cpu_to_le32(64);
+}
+
+void make_rndis_set_hdr(rndis_set_hdr *rndis_set) {
+	memset(rndis_set, 0, sizeof(rndis_set_hdr));
+	rndis_set->msg_type = cpu_to_le32(RNDIS_MSG_SET);
+	rndis_set->msg_len = cpu_to_le32(4 + sizeof(rndis_set_hdr));
+	rndis_set->oid = cpu_to_le32(RNDIS_OID_GEN_CURRENT_PACKET_FILTER);
+	rndis_set->len = cpu_to_le32(4);
+	rndis_set->offset = cpu_to_le32(20);
+	rndis_set->reserved = 0;
+	rndis_set->request_id = 23;
+
+}
+
+void debug_rndis(rndis_data_hdr *rndis) {
 	syslog(LOG_DEBUG, "STARGING RNDIS LOG");
 	syslog(LOG_DEBUG, "RNDIS MsgType: %d", rndis->msg_type);
 	syslog(LOG_DEBUG, "RNDIS MsgLen: %d", rndis->msg_len);
