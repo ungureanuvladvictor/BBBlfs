0307f937686a6c44fa1c37b4806a257a93789fa5
Vlad Ungureanu <vvu@vdev.ro>
2015-05-16 01:56:08 +0200

    Added license headers.
    Signed-off-by: Vlad Ungureanu <vvu@vdev.ro>

--- src/bootp.c
+++ src/bootp.c
@@ -1,17 +1,13 @@
 /*
- * Copyright 2013 Vlad V. Ungureanu <ungureanuvladvictor@gmail.com>.
+ * Copyright (C) 2013-2015 Vlad Ungureanu
+ * Contact: Vlad Ungureanu <vvu@vdev.ro>
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this Github repository and wiki except in
- * compliance with the License. You may obtain a copy of the License at
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 #include <syslog.h>
