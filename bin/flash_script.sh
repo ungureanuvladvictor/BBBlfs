891259b4ed39608b4dc0bb29438a8a6e0bd018cf
Nathan Dorfman <ndorf@rtfm.net>
2017-10-04 16:17:09 -0600

    Fix incorrect start sector.
    (After flashing bone-debian-9.1-iot-armhf-2017-09-21-4gb.img.xz, the
    only partition starts at sector 8192. The script was recreating it
    with the default value of 2048, making the filesystem unusable.)

--- bin/flash_script.sh
+++ bin/flash_script.sh
@@ -141,7 +141,7 @@ then
 			xzcat $input | sudo dd of=/dev/$bbb bs=1M
 			echo
 			echo "Resizing partitons now, just as a saefty measure if you flash 2GB image on 4GB board!"
-			echo -e "d\n2\nn\np\n2\n\n\nw" | sudo fdisk /dev/$bbb > /dev/null
+			echo -e "d\n2\nn\np\n2\n8192\n\nw" | sudo fdisk /dev/$bbb > /dev/null
 		fi
 		sudo e2fsck -f /dev/${bbb}2
 		sudo resize2fs /dev/${bbb}2
